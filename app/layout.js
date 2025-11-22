93dc0750252fc603b816bbcf0ea6782ff4eec524
FransisGIT <averangefransis@gmail.com>
2025-11-22 14:39:39 +0700

    (adjust) vercel analytic

--- app/layout.js
+++ app/layout.js
@@ -52,6 +52,7 @@ export default function RootLayout({ children }) {
             </head>
             <body>
                 {children}
+                <Analytics />
             </body>
         </html>
     );
